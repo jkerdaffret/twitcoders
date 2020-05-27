d1c23b9bf24dfab72dc5966c49d7bc9fb55f144c
Julien Kerdaffret <jkerdaffret@gmail.com>
2020-05-27 13:42:52 +0200

    first commit

---
+++ src/components/MessageList.js
@@ -0,0 +1,36 @@
+import React, { useContext, useState, useEffect } from 'react'
+import FirebaseContext from '../firebase/context';
+import firebase from '../firebase/index';
+import Message from './Message'
+
+const MessageList = () => {
+    const { firebase } = useContext(FirebaseContext)
+    const [messages, setMessages] = useState([])
+
+    const handleSnapshot = snapshot => {
+        const messages = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+        setMessages(messages)
+    }
+
+    useEffect(() => {
+        const getMessages = () => {
+            firebase.db
+            .collection('message')
+            .orderBy('createAt', 'desc')
+            .onSnapshot(handleSnapshot)
+        }
+
+        return getMessages()
+    }, [firebase])
+
+    return (
+        <div>
+            {
+                messages.map( message => (
+                <Message key={message.id} message={message} />
+                ))}
+        </div>
+    )
+}
+
+export default MessageList
---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCXkrTN7zzcKuaUkMj4_THkaPbYXEy0-Tc",
+    authDomain: "twitcoders-firebase.firebaseapp.com",
+    databaseURL: "https://twitcoders-firebase.firebaseio.com",
+    projectId: "twitcoders-firebase",
+    storageBucket: "twitcoders-firebase.appspot.com",
+    messagingSenderId: "1008916178666",
+    appId: "1:1008916178666:web:1c9210c8ef634c378f1993"
+  }
+
+  export default firebaseConfig
